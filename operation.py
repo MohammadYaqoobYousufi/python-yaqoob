636ebce69c39d0045a399c886d31032dc49f3a01
Mohammad Yaqoob Yousufi <armeen_yousufit@outlook.com>
2023-07-15 20:00:21 +0500

    added new file

--- operation.py
+++ operation.py
@@ -1,8 +1,24 @@
-carModel = 2000
-university = "UCA"
+# carModel = 2000
+# university = "UCA"
 
-print(carModel + 6)
-print(carModel * 34)
-print(carModel - 53)
-print(carModel / 11)
-print(university)
+# print(carModel + 6)
+# print(carModel * 34)
+# print(carModel - 53)
+# print(carModel / 11)
+# print(university)
+
+# # Your age in second
+# # your age in minutes
+# # your age in hours
+
+# print(36 * 365 * 24 * 60 * 60 )
+# print(36 * 365 * 24 * 60)
+# print(36 * 365 * 24 )
+
+# age = 36
+# daysInYear = 365
+# hoursInDay = 24
+# minutesInHour = 60
+# secondsInMinute = 60
+# print(age * daysInYear * hoursInDay * minutesInHour * secondsInMinute)
+
