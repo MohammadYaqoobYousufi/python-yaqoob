d18aace4209f7365138cc7092918480275d10a19
Mohammad Yaqoob Yousufi <mohdyaqoob.yousufi@gmail.com>
2023-07-18 19:36:26 +0500

    added all class work file

---
+++ dataType.py
@@ -0,0 +1,11 @@
+x = 20
+y = "30"
+
+print(x + int(x))
+
+z = 40
+j = "50"
+
+a = int(j)
+print(z+a)
+
---
+++ mutiplevalue.py
@@ -0,0 +1,8 @@
+x = 'Apple'
+y = 'Orange'
+z = 'banana'
+print(x,y,z)
+
+a, b, c = 'alpha', 'delta', 'mega'
+
+print(a)
