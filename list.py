af23cfda1adeafa8d5d99993902cc740eaf9f19f
Mohammad Yaqoob Yousufi <mohdyaqoob.yousufi@gmail.com>
2023-07-22 19:20:03 +0500

    list.py added

---
+++ list.py
@@ -0,0 +1,28 @@
+# x = 7 # integer
+# y = "University of central Asia" # string
+# z = "7" #string
+
+# # list
+
+# q  = [7, "University of central Asia", "7", "7", "Yaqoob","apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(type(q))
+# print(q)
+# print(q[1])
+# print(q[-1])
+# print(q[3:7])
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[2:5])
+# print(thislist[:4])
+# print(thislist[4:])
+
+AfghanistanProvinces = ["Kabul", "Samangan", "Balkh", "Polikhomri", "Charikar", "Parwan", "Kapisa", "Kundoz", "Saripol", "Nangarhar", "Jalalabda"]
+print(type(AfghanistanProvinces))
+print(AfghanistanProvinces[2])
+print(AfghanistanProvinces[3:7])
+print(AfghanistanProvinces[-1])
+print(AfghanistanProvinces[-4:-1])
+
+if ('Balkh') in AfghanistanProvinces:
+    print("Balkh, Exist") 
+
