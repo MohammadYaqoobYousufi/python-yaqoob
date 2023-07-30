d9365dd490116d97db389bd096e73b358081967d
Mohammad Yaqoob Yousufi <mohdyaqoob.yousufi@gmail.com>
2023-07-30 20:07:26 +0500

    lesson 30 July 2023 added

--- function.py
+++ function.py
@@ -1,4 +1,36 @@
-def my_function():
-  print("Hello from a function")
-  
+def my_function(country = "Afghaistan"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
 my_function()
+my_function("Brazil")
+
+
+
+
+# def google_map_project (**info):
+#     print("The project amount is"+" "+ info["budget"]+" "+"country is" +" "+ info["country"]+" " + "Number of Staff is "+" "+ info["staff"])
+
+# google_map_project(projectName = "google map", startDate = "July 30, 2023", budget ="1 million", staff = "1000", country = "USA")
+
+
+
+
+
+# def my_function():
+#   print("Hello from a function")
+  
+# my_function()
+
+# def my_function(fname, lname, address):
+#   print("The youngest child is " + fname)
+
+# my_function(fname = "Emil", lname = "Tobias", address = "Vahdat")
+
+# def country_province_city(country, province, city):
+#   print("My country is" + country)
+#   print("My province is" + province)
+#   print("my city is" + city)
+
+# country_province_city(country = "Afghanistan", province ="Kabul", city = "Kabul")
