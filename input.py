66956f18d69f5cb456718469a04994af3287fe93
Mohammad Yaqoob Yousufi <mohdyaqoob.yousufi@gmail.com>
2023-08-01 19:54:52 +0500

    input.py added

---
+++ input.py
@@ -0,0 +1,46 @@
+first = float (input("Enter your first number: "))
+second = float(input("Enter your second number: "))
+operation = input("Enter your operation (*, /, +, -) ")
+result = eval(f"{first} {operation} {second}")
+ 
+
+# First = input("enter number ")
+
+# Second = input("Enter number")
+
+# def Result ():
+#   return(Result)
+
+# print()
+
+
+
+
+
+# Revenue = input("Total Revenue for the year ")
+
+# Expenses = input("Total Expenses for the year ")
+
+# Net_profit = int (Revenue) - int (Expenses)
+
+# print("Net Profit is Equal to: " + str (Net_profit))
+
+
+
+# years = input = ("How many years you want to make in minutes ? ")
+
+# minutes = int(years) * 365 * 24 * 60
+
+# print(years + " years in equal to " + str(minutes) + " minutes")
+
+
+# name = input("What is your name? ")
+# print("Hello, "+ name)
+
+# address = input("where do you live ? ")
+# print("I live in " + address)
+
+# age = input("How old are you? " )
+# caculation = 2023 - int(age)
+
+# print("you were born on " + str(caculation))
